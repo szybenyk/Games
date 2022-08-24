9d950de1fcaf5505037a6fceabdfca55cde27117
Stanislaw Lubczynski <lubczyk.szybenyk@gmail.com>
2022-08-24 15:16:05 +0200

    Add function for generate random numbers

--- MonsterFight/include/Monster.h
+++ MonsterFight/include/Monster.h
@@ -4,6 +4,7 @@
 #include "Creature.h"
 
 #include <array>
+#include <random>
 
 
 class Monster : public Creature
@@ -30,6 +31,13 @@ class Monster : public Creature
                 { "slime", 's', 1, 1, 10 }
             }};
             return monsterData.at(static_cast<std::size_t>(type));
+        };
+
+        static int getRandomNumber(int min, int max)
+        {
+            static constexpr double fraction{ 1.0 / (RAND_MAX + 1.0) }; // static used for efficiency, so we only calculate this value once
+            // evenly distribute the random number across our range
+            return min + static_cast<int>((max - min + 1) * (std::rand() * fraction));
         };
 
 };
